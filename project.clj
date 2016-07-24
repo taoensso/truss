f30568ace9a8ae7fdb58da20c982fa57fb6d0a3b
Peter Taoussanis <ptaoussanis@gmail.com>
2016-07-24 14:39:39 +0700

    Update project template

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha9"]]}
+   :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}
    :test {:dependencies [[org.clojure/test.check "0.9.0"]]}
    :dev
    [:1.9 :test :server-jvm

9c7fb29e4099ad6bb3e823c672fc5dbdfabf4b8b
Peter Taoussanis <ptaoussanis@gmail.com>
2017-03-27 08:49:02 +0200

    Bump deps

--- project.clj
+++ project.clj
@@ -32,12 +32,12 @@
    :dev
    [:1.9 :test :server-jvm
     {:dependencies
-     [[org.clojure/clojurescript "1.9.473"]]
+     [[org.clojure/clojurescript "1.9.495"]]
 
      :plugins
      [;; These must be in :dev, Ref. https://github.com/lynaghk/cljx/issues/47:
       [com.keminglabs/cljx "0.6.0"]
-      [lein-cljsbuild      "1.1.3"]]}]}
+      [lein-cljsbuild      "1.1.5"]]}]}
 
   ;; :jar-exclusions [#"\.cljx|\.DS_Store"]
 

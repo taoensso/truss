9eee11ade82cfb7867c8193c2ea65486b3a26e87
Peter Taoussanis <peter@taoensso.com>
2025-02-17 14:15:35 +0100

    [mod] Drop Clojure v1.9 support
    This is now 3 versions back, and over 7 years old.
    
    Clojure v1.10 released: 2018-12-17 (~6 years ago)
    Clojure v1.9  released: 2017-12-08 (~7 years ago)

--- project.clj
+++ project.clj
@@ -17,7 +17,6 @@
    :c1.12    {:dependencies [[org.clojure/clojure       "1.12.0"]]}
    :c1.11    {:dependencies [[org.clojure/clojure       "1.11.4"]]}
    :c1.10    {:dependencies [[org.clojure/clojure       "1.10.3"]]}
-   :c1.9     {:dependencies [[org.clojure/clojure       "1.9.0"]]}
 
    :graal-tests
    {:source-paths ["test"]
@@ -71,6 +70,6 @@
    "build-once" ["do" ["clean"] ["cljsbuild" "once"]]
    "deploy-lib" ["do" ["build-once"] ["deploy" "clojars"] ["install"]]
 
-   "test-clj"   ["with-profile" "+c1.12:+c1.11:+c1.10:+c1.9" "test"]
-   "test-cljs"  ["with-profile" "+c1.12" "cljsbuild"         "test"]
+   "test-clj"   ["with-profile" "+c1.12:+c1.11:+c1.10" "test"]
+   "test-cljs"  ["with-profile" "+c1.12" "cljsbuild"   "test"]
    "test-all"   ["do" ["clean"] ["test-clj"] ["test-cljs"]]})

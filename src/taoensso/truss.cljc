taoensso/truss

commit 65d8f1747f3ba03532cdca3e1939b085177fd52e

[fix] `ex-info`: preserve generated data
Caller data could overwrite generated namespace and coordinate keys,
unlike the already-reserved Truss context.

Give all generated keys precedence and document the contract.

src/taoensso/truss.cljc
@@ -111,8 +111,8 @@
   (let [data-map
         (cond
           (nil? ns)                               data-map
-          coords   (conj {:ns ns, :coords coords} data-map)
-          :else    (conj {:ns ns}                 data-map))
+          coords   (assoc data-map :ns ns, :coords coords)
+          :else    (assoc data-map :ns ns))
 
         data-map
         (if-let [ctx *ctx*]
@@ -136,7 +136,9 @@
        `:ns` --------- Namespace string of ex-info callsite
        `:coords` ----- [line number]    of ex-info callsite, only present
                        if ex-info isn't wrapped by another macro (or see
-                      `keep-callsite` for a workaround)."
+                      `keep-callsite` for a workaround).
+
+     These generated keys take precedence over keys in the given data map."
      ([msg               ] `(ex-info* ~(str *ns*) ~(callsite-coords &form) ~msg nil       nil))
      ([msg data-map      ] `(ex-info* ~(str *ns*) ~(callsite-coords &form) ~msg ~data-map nil))
      ([msg data-map cause] `(ex-info* ~(str *ns*) ~(callsite-coords &form) ~msg ~data-map ~cause))))

test/taoensso/truss_tests.cljc
@@ -190,6 +190,18 @@
 
 (deftest _ex-info
   [(is (submap? (ex-data (truss/ex-info "msg")) {:ns string?, :coords vector?}))
+   (is (= :given (-> (truss/ex-info "msg" {:truss/ctx :given}) ex-data :truss/ctx)))
+   (is (submap?
+         (ex-data
+           (truss/with-ctx {:actual :ctx}
+             (truss/ex-info "msg" {:ns :given, :coords :given, :truss/ctx :given})))
+         {:ns string?, :coords vector?, :truss/ctx {:actual :ctx}}))
+   (is (submap?
+         (ex-data
+           (truss/with-ctx {:actual :ctx}
+             (throws
+               (truss/ex-info! "msg" {:ns :given, :coords :given, :truss/ctx :given}))))
+         {:ns string?, :coords vector?, :truss/ctx {:actual :ctx}}))
    (is (submap? (ex-data (truss/with-ctx {:c :c1} (truss/ex-info "msg")))          {:truss/ctx {:c :c1}}))
    (is (submap? (ex-data (truss/with-ctx {:c :c1} (truss/ex-info "msg" {:d :d1}))) {:truss/ctx {:c :c1}, :d :d1}))
    (is (submap? (ex-data

commit 752e41159ff329e70dbd6745726cf97b52afbfeb

[fix] `ex-info`: support data maps of arb type/keys
Fixes a regression introduced by 65d8f17, which assoc'd generated
keys directly into the given data map. This could throw for map
types with restricted keys, e.g.:

  (ex-info "msg" (sorted-map "a" 1)) ; v2.3.0 okay, but would now
  ;; throw CCE on trying to compare string and keyword keys

Generated keys are now injected into a plain map copy (matching
v2.3.x output type), and still take precedence over given keys.

src/taoensso/truss.cljc
@@ -108,16 +108,14 @@
 (defn ^:no-doc ex-info*
   "Private, don't use."
   [ns coords msg data-map cause]
-  (let [data-map
-        (cond
-          (nil? ns)                               data-map
-          coords   (assoc data-map :ns ns, :coords coords)
-          :else    (assoc data-map :ns ns))
-
+  (let [ctx *ctx*
         data-map
-        (if-let [ctx *ctx*]
-          (assoc data-map :truss/ctx ctx)
-          (do    data-map))]
+        (if (or ns ctx)
+          ;; Copy to plain map so that we can safely inject generated keys
+          ;; (which take precedence) into data maps of arb type/keys (sorted, etc.)
+          (impl/assoc-some (conj {} data-map)
+            {:ns ns, :coords (when ns coords), :truss/ctx (when ctx ctx)})
+          data-map)]
 
     (core/ex-info msg data-map cause)))
 

test/taoensso/truss_tests.cljc
@@ -207,6 +207,8 @@
 
 (deftest _ex-info
   [(is (submap? (ex-data (truss/ex-info "msg")) {:ns string?, :coords vector?}))
+   (is (submap? (ex-data (truss/ex-info "msg" (sorted-map "a" 1))) {"a" 1, :ns string?, :coords vector?})
+     "Safely inject generated keys into data maps of arb type/keys")
    (is (= :given (-> (truss/ex-info "msg" {:truss/ctx :given}) ex-data :truss/ctx)))
    (is (submap?
          (ex-data